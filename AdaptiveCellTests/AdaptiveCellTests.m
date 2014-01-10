1016fcca812867c9d6d0f0c81d3edcf910ac9f79
swinglife <swinglife@187.red-80-24-24.staticip.rima-tde.net>
2014-01-10 09:38:07 +0800

    Initial Commit

---
+++ AdaptiveCellTests/AdaptiveCellTests.m
@@ -0,0 +1,34 @@
+//
+//  AdaptiveCellTests.m
+//  AdaptiveCellTests
+//
+//  Created by swinglife on 14-1-10.
+//  Copyright (c) 2014年 swinglife. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AdaptiveCellTests : XCTestCase
+
+@end
+
+@implementation AdaptiveCellTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
